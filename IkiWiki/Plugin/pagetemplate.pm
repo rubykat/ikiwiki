2900f81bd2a5331d478f7c33ab9d8b23eafd013e
Kathryn Andersen <perlkat@katspace.org>
2011-08-07 20:15:02 +1000

    Pagetemplate takes a PageSpec.

--- IkiWiki/Plugin/pagetemplate.pm
+++ IkiWiki/Plugin/pagetemplate.pm
@@ -9,6 +9,7 @@ my %templates;
 
 sub import {
 	hook(type => "getsetup", id => "pagetemplate", call => \&getsetup);
+	hook(type => "checkconfig", id => "pagetemplate", call => \&checkconfig);
 	hook(type => "preprocess", id => "pagetemplate", call => \&preprocess);
 	hook(type => "templatefile", id => "pagetemplate", call => \&templatefile);
 }
@@ -19,6 +20,35 @@ sub getsetup () {
 			safe => 1,
 			rebuild => undef,
 		},
+		pagetemplate_matching => {
+			type => "string",
+			description => "list which page templates apply to which matching pages",
+			example => {
+				'discussion.tmpl' => '*/discussion',
+				'blog.tmpl' => 'blog/*',
+			},
+			advanced => 1,
+			safe => 1,
+			rebuild => 1,
+		},
+}
+
+sub checkconfig () {
+
+	while (my ($tmpl, $spec) = each %{$config{pagetemplate_matching}})
+	{
+	    if (! defined $tmpl ||
+		$tmpl !~ /^[-A-Za-z0-9._+]+$/ ||
+		! defined IkiWiki::template_file($tmpl))
+	    {
+		error sprintf(gettext("pagetemplate_matching: bad or missing template '%s'"), $tmpl)
+	    }
+	    if (!IkiWiki::pagespec_valid($spec))
+	    {
+		error sprintf(gettext("pagetemplate_matching: invalid pagespec '%s' for template '%s'"), $spec, $tmpl)
+	    }
+	}
+	return;
 }
 
 sub preprocess (@) {
@@ -40,8 +70,18 @@ sub preprocess (@) {
 sub templatefile (@) {
 	my %params=@_;
 
+	# Check for a template defined by a directive
 	if (exists $templates{$params{page}}) {
 		return $templates{$params{page}};
+	}
+
+	# Otherwise, check for a template pagespec
+	while (my ($tmpl, $spec) = each %{$config{pagetemplate_matching}})
+	{
+	    if (pagespec_match($page, $spec))
+	    {
+		return $tmpl;
+	    }
 	}
 	
 	return undef;
